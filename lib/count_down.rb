04c7e7e3ccf3e164f6b4be64ef7975abba57279a
Inga <ijaanimets@gmail.com>
2020-09-20 01:39:59 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,9 +1,9 @@
 # Write your code here
 magic_exit_number = 0
 i = 10
-while i > 0 do
+while i >= 1 do
   puts i
-  break if count == 1
+  break if count == magic_exit_number
   puts "Happy New Year!"
 
   i -= 1
